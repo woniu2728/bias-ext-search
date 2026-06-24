016084c9c8340ece0c4d26108f1b3486cd182b9c
woniu <2463396298@qq.com>
2026-06-24 12:14:33 +0800

    Add apps.py + django_migrations

---
+++ bias_ext_search/backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SearchExtensionConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bias_ext_search.backend"
+    label = "search"
